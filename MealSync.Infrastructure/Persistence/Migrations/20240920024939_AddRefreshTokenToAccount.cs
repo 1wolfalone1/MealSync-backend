43ad85424b43427a9141379d98dd081f68ae3ee8
vanthong <vanthong07012002@gmail.com>
2024-09-20 10:14:44 +0700

    MS-Add refresh token column

---
+++ MealSync.Infrastructure/Persistence/Migrations/20240920024939_AddRefreshTokenToAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealSync.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "refresh_token",
+                table: "account",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "refresh_token",
+                table: "account");
+        }
+    }
+}
