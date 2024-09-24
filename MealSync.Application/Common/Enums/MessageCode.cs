2dc4fa6076d46afd30f21f2ccafed4611dc02f8b
Phạm Hoàng Tiến <87059330+hoangtien2911@users.noreply.github.com>
2024-09-24 15:24:53 +0700

    MS -  Add email service

--- MealSync.Application/Common/Enums/MessageCode.cs
+++ MealSync.Application/Common/Enums/MessageCode.cs
@@ -18,4 +18,28 @@ public enum MessageCode
 
     [Description("E-Dormitory-NotFound")]
     E_DORMITORY_NOT_FOUND,
+
+    [Description("E-Account-PhoneNumberExist")]
+    E_ACCOUNT_PHONE_NUMBER_EXIST,
+
+    [Description("E-Account-EmailExist")]
+    E_ACCOUNT_EMAIL_EXIST,
+
+    [Description("I-Account-RegisterSuccessfully")]
+    I_ACCOUNT_REGISTER_SUCCESSFULLY,
+
+    [Description("I-Email-SubjectRegisterVerification")]
+    I_EMAIL_SUBJECT_REGISTER_VERIFICATION,
+
+    [Description("I-Email-RegisterVerification")]
+    I_EMAIL_REGISTER_VERIFICATION,
+
+    [Description("I-Email-ForgotPasswordVerification")]
+    I_EMAIL_FORGOT_PASSWORD_VERIFICATION,
+
+    [Description("I-Email-SubjectForgotPasswordVerification")]
+    I_EMAIL_SUBJECT_FORGOT_PASSWORD_VERIFICATION,
+
+    [Description("I-Account-Avatar")]
+    I_ACCOUNT_AVATAR,
 }
