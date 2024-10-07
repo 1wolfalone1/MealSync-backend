34d52596aa63e50ad758cba03599827fa71ea385
Phạm Hoàng Tiến <87059330+hoangtien2911@users.noreply.github.com>
2024-10-07 17:34:08 +0700

    MS - Hot fix register

--- MealSync.API/Controllers/FavouriteController.cs
+++ MealSync.API/Controllers/FavouriteController.cs
@@ -1,7 +1,9 @@
-﻿using MealSync.API.Shared;
+﻿using MealSync.API.Identites;
+using MealSync.API.Shared;
 using MealSync.Application.UseCases.Favourites.Commands.MarkFavourite;
 using MealSync.Application.UseCases.Favourites.Queries.FavouriteShop;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MealSync.API.Controllers;
 
@@ -9,12 +11,14 @@ namespace MealSync.API.Controllers;
 public class FavouriteController : BaseApiController
 {
     [HttpPut(Endpoints.FAVOURITE_SHOP)]
+    [Authorize(Roles = $"{IdentityConst.CustomerClaimName}")]
     public async Task<IActionResult> FavouriteShop(long id)
     {
         return HandleResult(await Mediator.Send(new MarkFavouriteCommand { ShopId = id }).ConfigureAwait(false));
     }
 
     [HttpGet(Endpoints.GET_FAVOURITE_SHOP)]
+    [Authorize(Roles = $"{IdentityConst.CustomerClaimName}")]
     public async Task<IActionResult> GetFavouriteShop(int pageIndex, int pageSize)
     {
         return HandleResult(await Mediator.Send(new GetFavouriteShopQuery
