07fa8b4a4e4196f6bf983f7c2bc5d04499195f46
vanthong <vanthong07012002@gmail.com>
2024-10-15 16:31:27 +0700

    MS - Hot Fix update and create response operating slot

---
+++ MealSync.Application/UseCases/OperatingSlots/Models/OperatingSlotResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MealSync.Application.UseCases.OperatingSlots.Models;
+
+public class OperatingSlotResponse
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; }
+
+    public int StartTime { get; set; }
+
+    public int EndTime { get; set; }
+}
