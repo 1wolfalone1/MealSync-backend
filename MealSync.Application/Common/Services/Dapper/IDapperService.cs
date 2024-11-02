eef95ee2d3ede66c81416f1d53ea55b7bf2e4d09
vanthong <vanthong07012002@gmail.com>
2024-11-03 01:27:15 +0700

    MS-313 - Api get all delivery package for each deliverier

--- MealSync.Application/Common/Services/Dapper/IDapperService.cs
+++ MealSync.Application/Common/Services/Dapper/IDapperService.cs
@@ -35,4 +35,5 @@ public enum QueryName
     GetListShopDeliveryStaffToAssign,
     GetListDeliveryDetailStasticByTimeFrame,
     GetListOrderUnAssignInTimeFrame,
+    GetDeliveryPackageStasisticById,
 }
