3435e44c54ab728c8df8eafd2f9b23e7d4a4f7d6
vanthong <vanthong07012002@gmail.com>
2024-11-07 21:14:34 +0700

    MS - Hotfix - get list delivery package id

---
+++ MealSync.Application/UseCases/DeliveryPackages/Models/DeliveryPackageGroupDetailForWebResponse.cs
@@ -0,0 +1,32 @@
+﻿using MealSync.Application.UseCases.Orders.Models;
+
+namespace MealSync.Application.UseCases.DeliveryPackages.Models;
+
+public class DeliveryPackageGroupDetailForWebResponse
+{
+    public long Id
+    {
+        get
+        {
+            return DeliveryPackageId;
+        }
+    }
+
+    public long DeliveryPackageId { get; set; }
+
+    public int Total { get; set; }
+
+    public int Waiting { get; set; }
+
+    public int Delivering { get; set; }
+
+    public int Successful { get; set; }
+
+    public int Failed { get; set; }
+
+    public DeliveryPackageGroupDetailResponse.ShopStaffInforInDelvieryPackage ShopDeliveryStaff { get; set; }
+
+    public List<DeliveryPackageGroupDetailResponse.DormitoryStasisticForEachStaff> Dormitories { get; set; } = new();
+
+    public List<OrderForShopByStatusResponse> Orders { get; set; } = new();
+}
