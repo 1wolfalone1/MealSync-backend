f9d4b00ce98f4def67b5f21d439ffeac5669f4fc
vanthong <vanthong07012002@gmail.com>
2024-11-07 09:23:47 +0700

    MS-Hotfix datetime format iso

---
+++ MealSync.API/Converters/DateTimeOffsetConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MealSync.API.Converters;
+
+public class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+{
+    public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.GetDateTimeOffset();
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+    {
+        // Convert to UTC and format in ISO 8601 with 'Z' for UTC time
+        writer.WriteStringValue(value.UtcDateTime.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"));
+    }
+}
