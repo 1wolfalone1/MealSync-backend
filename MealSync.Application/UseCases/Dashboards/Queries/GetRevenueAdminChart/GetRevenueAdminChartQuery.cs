2e5b79ba7c5c47630b0ad861b33e14c665aae274
vanthong <vanthong07012002@gmail.com>
2024-11-18 02:01:57 +0700

    MS-365 - Api get revenue for admin

---
+++ MealSync.Application/UseCases/Dashboards/Queries/GetRevenueAdminChart/GetRevenueAdminChartQuery.cs
@@ -0,0 +1,9 @@
+﻿using MealSync.Application.Common.Abstractions.Messaging;
+using MealSync.Application.Shared;
+
+namespace MealSync.Application.UseCases.Dashboards.Queries.GetRevenueAdminChart;
+
+public class GetRevenueAdminChartQuery : IQuery<Result>
+{
+    public DateTime DateOfYear { get; set; } = DateTime.Now;
+}
