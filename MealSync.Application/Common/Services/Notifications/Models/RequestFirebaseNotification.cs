af56de5c5d9fdd0aba498df012bbcef61f159813
vanthong <vanthong07012002@gmail.com>
2024-11-21 17:13:45 +0700

    MS-380 - Kafka consumer notification

---
+++ MealSync.Application/Common/Services/Notifications/Models/RequestFirebaseNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace MealSync.Application.Common.Services.Notifications.Models;
+
+public class RequestFirebaseNotification
+{
+    public long FromAccountId { get; set; }
+
+    public long ToAccountId { get; set; }
+}
