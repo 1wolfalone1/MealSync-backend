d6d56ce68a93df203218e16d313d807d838c8481
vanthong <vanthong07012002@gmail.com>
2024-11-26 19:42:11 +0700

    MS-396 - Api for get notification and unread notification

---
+++ MealSync.Application/Mappings/NotificationCustomerResponseMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MealSync.Application.UseCases.Notifications.Models;
+using MealSync.Domain.Entities;
+
+namespace MealSync.Application.Mappings;
+
+public class NotificationCustomerResponseMapping : Profile
+{
+    public NotificationCustomerResponseMapping()
+    {
+        CreateMap<Notification, NotificationCustomerResponse.NotificationInfor>()
+            .ForMember(dest => dest.CreatedDateTime, opt => opt.MapFrom(
+                src => src.CreatedDate))
+            .ForMember(dest => dest.UpdatedDateTime, opt => opt.MapFrom(
+                src => src.UpdatedDate));
+    }
+}
