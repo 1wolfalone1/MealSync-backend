81d9c328900f6096321c629b50042800f3a4c0de
vanthong <vanthong07012002@gmail.com>
2024-12-06 09:47:10 +0700

    MS - Fix report case when delivery fail

--- MealSync.Application/UseCases/Dashboards/Queries/GetOrderChartForAdmin/GetOrderChartForAdminHandler.cs
+++ MealSync.Application/UseCases/Dashboards/Queries/GetOrderChartForAdmin/GetOrderChartForAdminHandler.cs
@@ -52,12 +52,13 @@ public class GetOrderChartForAdminHandler : IQueryHandler<GetOrderChartForAdminQ
             DateFrom = dateStep.ToString("yyyy-MM-dd"),
             DateTo = dateStep.ToString("yyyy-MM-dd"),
             PaymentOnlineList = new PaymentMethods[] { PaymentMethods.VnPay },
-            DeliveryFailReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER_REPORTED_BY_CUSTOMER,
-            DeliveredReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERED_REPORTED_BY_CUSTOMER,
-            DeliveryFailByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER,
-            CustomerCancel = OrderIdentityCode.ORDER_IDENTITY_CUSTOMER_CANCEL,
-            ShopCancel = OrderIdentityCode.ORDER_IDENTITY_SHOP_CANCEL,
-            DeliveryFailByShop = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP,
+            DeliveryFailByShopReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP_REPORTED_BY_CUSTOMER.GetDescription(),
+            DeliveryFailByCustomerReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER_REPORTED_BY_CUSTOMER.GetDescription(),
+            DeliveredReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERED_REPORTED_BY_CUSTOMER.GetDescription(),
+            DeliveryFailByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER.GetDescription(),
+            CustomerCancel = OrderIdentityCode.ORDER_IDENTITY_CUSTOMER_CANCEL.GetDescription(),
+            ShopCancel = OrderIdentityCode.ORDER_IDENTITY_SHOP_CANCEL.GetDescription(),
+            DeliveryFailByShop = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP.GetDescription(),
         });
 
         return point;
--- MealSync.Application/UseCases/Dashboards/Queries/GetRevenueAdminChart/GetRevenueAdminChartHandler.cs
+++ MealSync.Application/UseCases/Dashboards/Queries/GetRevenueAdminChart/GetRevenueAdminChartHandler.cs
@@ -24,12 +24,13 @@ public class GetRevenueAdminChartHandler : IQueryHandler<GetRevenueAdminChartQue
             {
                 DateOfYear = request.DateOfYear,
                 PaymentOnlineList = new PaymentMethods[] { PaymentMethods.VnPay },
-                DeliveryFailReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER_REPORTED_BY_CUSTOMER,
-                DeliveredReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERED_REPORTED_BY_CUSTOMER,
-                DeliveryFailByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER,
-                CustomerCancel = OrderIdentityCode.ORDER_IDENTITY_CUSTOMER_CANCEL,
-                ShopCancel = OrderIdentityCode.ORDER_IDENTITY_SHOP_CANCEL,
-                DeliveryFailByShop = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP,
+                DeliveryFailByShopReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP_REPORTED_BY_CUSTOMER.GetDescription(),
+                DeliveryFailByCustomerReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER_REPORTED_BY_CUSTOMER.GetDescription(),
+                DeliveredReportedByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERED_REPORTED_BY_CUSTOMER.GetDescription(),
+                DeliveryFailByCustomer = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_CUSTOMER.GetDescription(),
+                CustomerCancel = OrderIdentityCode.ORDER_IDENTITY_CUSTOMER_CANCEL.GetDescription(),
+                ShopCancel = OrderIdentityCode.ORDER_IDENTITY_SHOP_CANCEL.GetDescription(),
+                DeliveryFailByShop = OrderIdentityCode.ORDER_IDENTITY_DELIVERY_FAIL_BY_SHOP.GetDescription(),
             }).ConfigureAwait(false);
         return Result.Success(revenueChart);
     }
