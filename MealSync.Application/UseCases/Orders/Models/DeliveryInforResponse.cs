a7810df381cb26d9ed98f50f8e8d755b3d4adc99
vanthong <vanthong07012002@gmail.com>
2024-12-06 10:41:49 +0700

    MS - Delivery Succes evedence

--- MealSync.Application/UseCases/Orders/Models/DeliveryInforResponse.cs
+++ MealSync.Application/UseCases/Orders/Models/DeliveryInforResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace MealSync.Application.UseCases.Orders.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace MealSync.Application.UseCases.Orders.Models;
 
 public class DeliveryInforResponse
 {
@@ -9,6 +11,22 @@ public class DeliveryInforResponse
     public DateTimeOffset ReceiveAt { get; set; }
 
     public bool IsDeliveredByQR { get; set; }
+
+    [JsonIgnore]
+    public string DeliverySuccessImageUrl { get; set; }
+
+    public string[] DeliverySuccessImageUrls
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(DeliverySuccessImageUrl))
+            {
+                return DeliverySuccessImageUrl.Split(",");
+            }
+
+            return new string[0];
+        }
+    }
 
     public int DeliveryStatus { get; set; }
 
