e4d0c8d532a4db60f0b05b074b557977ab40639a
vanthong <vanthong07012002@gmail.com>
2024-12-07 14:14:54 +0700

    MS - Api Update FPU

---
+++ MealSync.Application/UseCases/FoodPackingUnits/Commands/Update/ShopUpdateFPU/ShopUpdateFPUCommand.cs
@@ -0,0 +1,13 @@
+﻿using MealSync.Application.Common.Abstractions.Messaging;
+using MealSync.Application.Shared;
+
+namespace MealSync.Application.UseCases.FoodPackingUnits.Commands.Update.ShopUpdateFPU;
+
+public class ShopUpdateFPUCommand : ICommand<Result>
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public double Weight { get; set; }
+}
